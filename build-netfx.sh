53153518d816ecf0827bd57ebdf035bc21e38c68
Allister Beharry <allisterb@users.noreply.github.com>
2018-02-04 18:05:38 -0400

    Update build-netfx.sh

--- build-netfx.sh
+++ build-netfx.sh
@@ -3,4 +3,4 @@
 set -e
 
 rm -rf ./Alpheus.CommandLine/bin/Debug/Examples/*
-nuget restore Alpheus.sln && msbuild Alpheus.sln /p:Configuration=NetFxD $*
+nuget restore Alpheus.sln && msbuild Alpheus.sln /p:Configuration=Debug $*
